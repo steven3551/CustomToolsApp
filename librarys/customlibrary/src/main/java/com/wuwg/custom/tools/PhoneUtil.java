36355e0b8e5b522d5b130dcabae3e5f6b2384138
wuwg <wuwg@visionet.com.cn>
2017-06-15 12:44:24 +0800

    [added] 1、添加常见工具类

---
+++ librarys/customlibrary/src/main/java/com/wuwg/custom/tools/PhoneUtil.java
@@ -0,0 +1,126 @@
+package com.wuwg.custom.tools;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+
+import java.io.File;
+
+/**
+ * 手机组件调用工具类
+ *
+ * @author jingle1267@163.com
+ */
+public final class PhoneUtil {
+    private static long lastClickTime;
+
+    /**
+     * Don't let anyone instantiate this class.
+     */
+    private PhoneUtil() {
+        throw new Error("Do not need instantiate!");
+    }
+
+
+    /**
+     * 调用系统发短信界面
+     *
+     * @param activity    Activity
+     * @param phoneNumber 手机号码
+     * @param smsContent  短信内容
+     */
+    public static void sendMessage(Context activity, String phoneNumber, String smsContent) {
+        if (phoneNumber == null || phoneNumber.length() < 4) {
+            return;
+        }
+        Uri uri = Uri.parse("smsto:" + phoneNumber);
+        Intent it = new Intent(Intent.ACTION_SENDTO, uri);
+        it.putExtra("sms_body", smsContent);
+        it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(it);
+    }
+
+    /**
+     * 调用拨打电话界面
+     *
+     * @param context 上下文
+     * @param phoneNumber 电话号码
+     */
+    public static void toCallPhone(Context context, String phoneNumber) {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        Uri data = Uri.parse("tel:" + phoneNumber);
+        intent.setData(data);
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        context.startActivity(intent);
+    }
+
+
+    /**
+     * 判断是否为连击
+     *
+     * @return boolean
+     */
+    public static boolean isFastDoubleClick() {
+        long time = System.currentTimeMillis();
+        long timeD = time - lastClickTime;
+        if (0 < timeD && timeD < 500) {
+            return true;
+        }
+        lastClickTime = time;
+        return false;
+    }
+
+    /**
+     * 拍照打开照相机！
+     *
+     * @param requestcode 返回值
+     * @param activity    上下文
+     * @param fileName    生成的图片文件的路径
+     */
+    public static void toTakePhoto(int requestcode, Activity activity, String fileName) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra("camerasensortype", 2);// 调用前置摄像头
+        intent.putExtra("autofocus", true);// 自动对焦
+        intent.putExtra("fullScreen", false);// 全屏
+        intent.putExtra("showActionIcons", false);
+        try {//创建一个当前任务id的文件然后里面存放任务的照片的和路径！这主文件的名字是用uuid到时候在用任务id去查路径！
+            File file = new File(fileName);
+            if (!file.exists()) {//如果这个文件不存在就创建一个文件夹！
+                file.mkdirs();
+            }
+            Uri uri = Uri.fromFile(new File(fileName));
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+            activity.startActivityForResult(intent, requestcode);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 打开相册
+     *
+     * @param requestcode 响应码
+     * @param activity    上下文
+     */
+    public static void toTakePicture(int requestcode, Activity activity) {
+        Intent intent = new Intent(Intent.ACTION_PICK, null);
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                "image/*");
+        activity.startActivityForResult(intent, requestcode);
+    }
+}
